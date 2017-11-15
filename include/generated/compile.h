064278791075010692c8154fb30f617b2c8ece1e
CJ Laing <dreamcaster23@gmail.com>
2017-11-15 05:44:19 -0500

    Cleaned up, patched timeconst.pl and made this monster buildable again!

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 6 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP PREEMPT Tue Jun 14 16:29:40 EDT 2016"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "toxus-LENOVO-IdeaPad-Z585"
-#define LINUX_COMPILER "gcc version 4.9.2 (Ubuntu/Linaro 4.9.2-10ubuntu13) "
